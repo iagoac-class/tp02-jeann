9bc523695b0b9499e7886dbae509dc17d4234432
JeannVictor <jeann19x@gmail.com>
2024-10-11 10:51:15 -0300

    nicola burrão

--- arvores.c
+++ arvores.c
@@ -4,6 +4,52 @@
 #include <time.h>       
 #include "arvores.h"
 
+typedef struct noArvore_ noArvore;
+struct noArvore_ {
+    int valor;
+    noArvore * esq;
+    noArvore * dir;
+};
+
+noArvore * busca (noArvore * raiz, int k) {
+if (raiz == NULL || raiz->valor == k)
+    return raiz;
+if (raiz->valor > k)
+    return busca (raiz->esq, k);
+else
+    return busca (raiz->dir, k);
+}
+
+noArvore * insere (noArvore * raiz, noArvore * n) {
+    if (raiz == NULL) return n;
+    if (raiz->valor > n->valor)
+        raiz->esq = insere(raiz->esq, n);
+    else
+        raiz->dir = insere (raiz->dir, n);
+    return raiz;
+}
+noArvore *buscaPai(noArvore * raiz,noArvore * valor) {
+       
+}
+
+noArvore * removeNo(noArvore * raiz, int valor) {
+    noArvore * n = busca(raiz, valor); //checa se o noh existe
+    if (n) {
+        noArvore * pai = buscaPai(raiz, n); //descobre quem eh o noh pai
+        if (pai) { //caso tenha noh pai
+            if (pai->dir == n) //noh a ser removido eh filho a direita
+                pai->dir = removeRaiz(n);
+            else //noh a ser removido eh filho a esquerda
+                pai->esq = removeRaiz(n);
+        } else { //nao possui pai, logo, eh o proprio noh raiz
+            raiz = removeRaiz(n);
+        }
+    }
+    return raiz;
+}
+
+
+
 double arvore_binaria(int instancia_num) {
     double tempo = 0;
     clock_t begin = clock();
